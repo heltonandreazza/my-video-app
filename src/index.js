44f0ef620c13d02a4b4741dfbe0b04c051db8a48
Helton Andreazza <helton.prg@gmail.com>
2017-08-08 08:41:15 -0300

    created video list, video list item, video details, and added to index

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAZwh3BG7evVXgCPte72EeebIzrryyKJKg';
 
-// create new component. this component should 
-// produce some html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: [] };
+    
+    YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
-// take this components html generated and put it
-// on the page (in the DOM)
 ReactDom.render(<App />, document.querySelector('.container'));
