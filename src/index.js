bc0519be41cbcbd7edbd65ac9d5850466aac696d
Helton Andreazza <helton.prg@gmail.com>
2017-08-07 23:31:30 -0300

    added youtube api v3 key and added youtube api search depedency

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+const API_KEY = 'AIzaSyAZwh3BG7evVXgCPte72EeebIzrryyKJKg';
+
 // create new component. this component should 
 // produce some html
 const App = () => {
